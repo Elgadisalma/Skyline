851a34d2b0e3c9ff3879e3f9ec39fc2b0a7e9664
Elgadisalma <elgadi.salma0@gmail.com>
2024-06-26 12:32:51 +0100

    vehicule class with attributes

---
+++ OOP/Vehicule.php
@@ -0,0 +1,15 @@
+<?php
+
+class Vehicule implements Details{
+
+    private $matricule;
+    private $type;
+    private $proprietaire;
+
+
+
+    public function afficherDetails()
+    {
+        echo "salma";
+    }
+}
